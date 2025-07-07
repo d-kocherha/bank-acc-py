36ed93a11c5e41e492f3e0636ee0b6ef5187e4c0
Dmytro Kocherha <dm.koch19@gmail.com>
2025-07-07 17:48:39 +0200

    Removed classes with use of mock database. Learning SQL, will see how can use it later on.

--- user/user_class.py
+++
@@ -1,9 +0,0 @@
-class User:
-    """Initialize user class to register, login (more to come)."""
-
-    def __init__(self, user_name, password):
-        self.user_name = user_name
-        self.password = password
-        self.unique_id = []
-        self.access = True
-        
