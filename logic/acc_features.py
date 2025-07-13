be9de0d6e1598f675e29f5a3e233649e0c84425a
Dima <dm.koch@gmail.com>
2025-07-13 14:52:33 +0200

    Refactored functional approach to object. Funtions tested and ready to be implemented in the app.

--- logic/acc_features.py
+++ logic/acc_features.py
@@ -1,21 +1,35 @@
-def create_account(user_ID):
-    """Function for creation of user bank account"""
-    user_account = {user_ID : {"balance" : 0}}     
-    return user_account
 
-def retrieve_balance(account_data):
-    """Fetch data from accounts for balance upd."""
-    pass
+class BankAccount:
 
-def withdrawal(balance, withdrawal):
-    # Withdrawal works okay. No changes required.
-    balance -= withdrawal
-    print(f"You have withdrawn {float(withdrawal)}$.\n"\
-        f"Your new balance is {float(balance)}$.")
+    def __init__(self):
+        self.balance = 0
+
+    def create_account(self, user_ID):
+        """Function for creation of user bank account"""
+        user_account = {user_ID : {"balance" : 0}}     
+        return user_account
+
+    def retrieve_balance(account_data):
+        """Fetch data from accounts for balance upd."""
+        pass
+
+    def withdrawal(self, withdrawal):
+        # Withdrawal works okay. No changes required.
+        if self.balance-withdrawal < 0:
+            print("You don't have enough for withdrawal!")
+            print(f"Current balance : {self.balance}")
+        else:
+            self.balance -= withdrawal
+            print(f"You have withdrawn {float(withdrawal)}$.\n"\
+                f"Your new balance is {float(self.balance)}$.")
+            return self.balance
+
+    def deposit(self, deposit):
+        # Deposit works okay. No changes required.
+        self.balance += deposit
+        print(f"Your deposit of {float(deposit)}$ has been successful.\n"\
+            f"Your new balance is {float(self.balance)}$")
+        return self.balance
+    
 
 
-def deposit(balance, deposit):
-    # Deposit works okay. No changes required.
-    balance += deposit
-    print(f"Your deposit of {float(deposit)}$ has been successful.\n"\
-          f"Your new balance is {float(balance)}$")
