ff09482290c2bf1d7d36bb24837dc01ddd931c0f
Dima <dm.koch@gmail.com>
2025-07-20 18:36:51 +0200

    Created methods to access and update history file.

--- data/db_operator.py
+++ data/db_operator.py
@@ -3,17 +3,17 @@ from pathlib import Path
 
 class Data_Operator:
 
-    def __init__(self, user_db, account_db):
+    def __init__(self, user_db, account_db, user_history):
         self.user_db = Path(f"{user_db}.json")
         self.account_db = Path(f"{account_db}.json")
+        self.user_history = Path(f"{user_history}.json")
 
     def read_db(self):
-        """Read db to return values for further search."""
-        db = json.loads(self.user_db.read_text()) #convert db to dictionary
+        db = json.loads(self.user_db.read_text())
         return db
 
     def update_db(self, user_data):
-        """Save changes to db"""
+        """Save changes to users"""
         db = self.read_db()
         updated = {**db, **user_data}
         return self.user_db.write_text(json.dumps(updated, indent=4))
@@ -36,10 +36,26 @@ class Data_Operator:
             return balance
         else:
             print("There's no bank account for this user yet.")
-        
+    
     def update_balance(self, upd_balance, user_id):
-        "Attempt to create function to change "
+        "Method to update balance in acc_db.json"
         open_db = self.read_acc()
         if user_id in open_db:
             open_db[user_id]["balance"] = upd_balance
             return self.account_db.write_text(json.dumps(open_db, indent=4))
+    
+    def read_history(self):
+        open_db_history = json.loads(self.user_history.read_text())
+        return open_db_history
+
+    def update_history(self, user_id, new_entry):
+        """Method to push transaction to the history list"""
+        open_db = self.read_history()
+        if user_id in open_db:
+            open_db[user_id].append(new_entry)
+        else:
+            open_db[user_id] = [new_entry]
+        return self.user_history.write_text(json.dumps(open_db, indent=4))
+
+
+    
